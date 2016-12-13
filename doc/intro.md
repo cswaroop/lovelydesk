7df17adf03161f2a3915cdf006cb1d6b238f2f2b
Swaroop Chalasani <SwaroopChalasani@corp.fairisaac.com>
2016-12-13 19:57:11 +0530

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lovelydesk
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/lovelydesk/core_test.clj
@@ -0,0 +1,7 @@
+(ns lovelydesk.core-test
+  (:require [clojure.test :refer :all]
+            [lovelydesk.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
